e6648e094ed34a4dd6dc7e41be95979dd90c71d8
Francisca <francisca.orwa@student.moringaschool.com>
2022-11-29 20:13:17 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form=document.querySelector("#create-task-form");
+  let input=document.querySelector("#new-task-description")
+  let list=document.querySelector("#tasks")
+  form.addEventListener('submit',function(e){
+    e.preventDefault()
+    if(input.value){
+      let tasks=input.value;
+      let newLi=document.createElement("li")
+      newLi.innerText=tasks;
+      list.append(newLi);
+      input.value = '';
+    }
+  });
 });
